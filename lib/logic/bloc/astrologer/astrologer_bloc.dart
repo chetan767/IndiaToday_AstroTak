5755e28917cba77af06e030a24d38f1935f7d2e3
Chetan Khanna <chetankhanna767@gmail.com>
2021-12-23 09:17:31 +0530

    clean code

--- lib/logic/bloc/astrologer/astrologer_bloc.dart
+++ lib/logic/bloc/astrologer/astrologer_bloc.dart
@@ -18,23 +18,15 @@ class AstrologerBloc extends Bloc<AstrologerEvent, AstrolgerState> {
     AstrologerEvent event,
   ) async* {
     if (event is AstrologerGetData) {
-      print("www");
-      print("start api");
-
       AstrolgerState astrolgerState = await respository.getData();
-      print("done api");
 
-      print(astrolgerState.message);
       String languages = "";
       yield (state.copyWith(
           data: astrolgerState.data, filteredData: astrolgerState.data));
-
-      print("done state");
     } else if (event is AstrologerFilterList) {
       if (event.text == "") {
         yield (state.copyWith(filteredData: state.data));
       } else {
-        print(event.text);
         List<Data> filter = state.data.toList();
         List<Data> filter_list1 = filter
             .where((element) => (element.firstName + " " + element.lastName)
@@ -60,34 +52,23 @@ class AstrologerBloc extends Bloc<AstrologerEvent, AstrolgerState> {
                 .contains(event.text.toLowerCase()))
             .toList();
 
-        print(filter);
         filter_list1.addAll(filter_list2);
         filter_list1.addAll(filter_list3);
 
         yield (state.copyWith(filteredData: filter_list1.toList()));
       }
     } else if (event is AstologerShowSearch) {
-      print(event.show);
-      print("xyz");
       if (event.show == false) {
         yield (state.copyWith(filteredData: state.data));
       }
       yield (state.copyWith(showSearch: event.show));
-      print("show : " + state.showSearch.toString());
     } else if (event is AstrologerFilterSkill) {
-      print("ww");
       var list = event.skills.toList();
 
       yield state.copyWith(skill_values: list);
-
-      print(state.skill_values);
     } else if (event is AstrologerFilterLang) {
-      print(event.lang);
       yield state.copyWith(lang_values: event.lang);
-      print(state.lang_values);
     } else if (event is AstrologerFilterBy) {
-      print(state.skill_values);
-
       List<Data> filter_list = state.data.toList();
 
       List<Skill> skills = state.skill_values;
@@ -98,7 +79,6 @@ class AstrologerBloc extends Bloc<AstrologerEvent, AstrolgerState> {
           .where((element) => element.skills.toSet().containsAll(skills))
           .toList();
 
-      print(filter_list1);
       List<Data> filter_list2 = filter_list1
           .where((element) => element.languages.toSet().containsAll(langs))
           .toList();
